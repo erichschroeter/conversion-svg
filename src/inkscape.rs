aef7740165e269a2a5ddc82feac9348580828a15
Erich Schroeter <erich.schroeter@gmail.com>
2024-05-24 08:25:59 -0500

    refactor: moved Inkscape structs to inkscape.rs

--- src/inkscape.rs
+++ src/inkscape.rs
@@ -1,11 +1,159 @@
-use std::path::Path;
-
-use crate::{InkscapeArgs, InkscapeArgsBuilder, InkscapeCmd};
+use std::path::{Path, PathBuf};
+use regex::Regex;
 
 use super::AppUI;
 use futures::{future::{Fuse, FusedFuture, FutureExt}, pin_mut, select};
 use slint::ComponentHandle;
 use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
+
+#[derive(Debug)]
+pub struct InkscapeCmd {
+    exe_path: PathBuf,
+    args: InkscapeArgs,
+}
+
+impl InkscapeCmd {
+    pub fn new(exe_path: PathBuf, args: InkscapeArgs) -> Self {
+        Self { exe_path, args }
+    }
+
+    pub fn as_command(&self) -> std::process::Command {
+        let mut cmd = std::process::Command::new(&self.exe_path);
+        match (self.args.export_png, self.args.export_pdf, self.args.export_eps) {
+            (true, true, true)   => cmd.arg("--export-type=png,pdf,eps"),
+            (true, true, false)  => cmd.arg("--export-type=png,pdf"),
+            (true, false, true)  => cmd.arg("--export-type=png,eps"),
+            (true, false, false) => cmd.arg("--export-type=png"),
+            (false, true, true)  => cmd.arg("--export-type=pdf,eps"),
+            (false, true, false) => cmd.arg("--export-type=pdf"),
+            (false, false, true) => cmd.arg("--export-type=eps"),
+            (false, false, false) => cmd.arg(""),
+        };
+        if let Some(input_path) = &self.args.file_path_input {
+            cmd.arg(input_path);
+        }
+        cmd
+    }
+
+    pub fn exec(&self) {
+        let mut cmd = self.as_command();
+        log::debug!("RUNNING!");
+        cmd.spawn().expect("Failed to execute inkscape");
+    }
+}
+
+impl From<std::process::Command> for InkscapeCmd {
+    fn from(cmd: std::process::Command) -> Self {
+        let exe_path = cmd.get_program().to_owned();
+        let args = cmd.get_args().into();
+        Self { exe_path: exe_path.into(), args }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct InkscapeArgs {
+    file_path_input: Option<String>,
+    // file_path_output: Option<String>,
+    export_png: bool,
+    export_eps: bool,
+    export_pdf: bool,
+}
+
+impl Default for InkscapeArgs {
+    fn default() -> Self {
+        InkscapeArgs {
+            file_path_input: None,
+            // file_path_output: None,
+            export_png: false,
+            export_eps: false,
+            export_pdf: false,
+        }
+    }
+}
+
+impl std::fmt::Display for InkscapeArgs {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "inkscape")?;
+        match (self.export_png, self.export_pdf, self.export_eps) {
+            (true, true, true) => write!(f, " --export-type=png,pdf,eps"),
+            (true, true, false) => write!(f, " --export-type=png,pdf"),
+            (true, false, true) => write!(f, " --export-type=png,eps"),
+            (true, false, false) => write!(f, " --export-type=png"),
+            (false, true, true) => write!(f, " --export-type=pdf,eps"),
+            (false, true, false) => write!(f, " --export-type=pdf"),
+            (false, false, true) => write!(f, " --export-type=eps"),
+            (false, false, false) => write!(f, ""),
+        }
+    }
+}
+
+impl<'a> From<std::process::CommandArgs<'a>> for InkscapeArgs {
+    fn from(args: std::process::CommandArgs) -> Self {
+        let re_export_type = Regex::new(r"--export-type=(?<types>.*)").unwrap();
+        let mut cmd = InkscapeArgs::default();
+        for arg in args {
+            if let Some(caps) = re_export_type.captures(arg.to_str().unwrap()) {
+                let types = caps.name("types").unwrap().as_str();
+                for t in types.split(",") {
+                    match t {
+                        "png" => cmd.export_png = true,
+                        "eps" => cmd.export_eps = true,
+                        "pdf" => cmd.export_pdf = true,
+                        _ => {}
+                    }
+                }
+            }
+        }
+        cmd
+    }
+}
+
+#[derive(Debug)]
+pub struct InkscapeArgsBuilder {
+    #[allow(dead_code)]
+    file_path_input: Option<String>,
+    cmd: InkscapeArgs,
+}
+
+impl Default for InkscapeArgsBuilder {
+    fn default() -> Self {
+        InkscapeArgsBuilder {
+            file_path_input: None,
+            cmd: InkscapeArgs::default(),
+        }
+    }
+}
+
+impl InkscapeArgsBuilder {
+    pub fn new() -> Self {
+        InkscapeArgsBuilder::default()
+    }
+
+    pub fn input_file(mut self, file_path_input: &str) -> Self {
+        self.cmd.file_path_input = Some(file_path_input.to_owned());
+        self
+    }
+
+    pub fn png(&mut self, enabled: bool) -> &mut Self {
+        self.cmd.export_png = enabled;
+        self
+    }
+
+    pub fn eps(&mut self, enabled: bool) -> &mut Self {
+        self.cmd.export_eps = enabled;
+        self
+    }
+
+    pub fn pdf(&mut self, enabled: bool) -> &mut Self {
+        self.cmd.export_pdf = enabled;
+        self
+    }
+
+    #[allow(dead_code)]
+    pub fn build(&self) -> InkscapeArgs {
+        self.cmd.clone()
+    }
+}
 
 #[derive(Debug)]
 pub enum InkscapeMessage {
@@ -46,6 +194,7 @@ async fn inkscape_worker_loop(
     handle: slint::Weak<AppUI>,
 ) {
 // ) -> tokio::io::Result<()> {
+    // TODO search for Inkscape on the PATH or via known installation locations
     // let run_inkscape_future = Fuse::terminated();
     // pin_mut!(run_inkscape_future);
     loop {
@@ -75,4 +224,55 @@ async fn run_inkscape(handle: slint::Weak<AppUI>) {
     // let _ = cmd.wait();
     let cmd = InkscapeCmd::new(Path::new("inkscape").into(), args.build());
     cmd.exec();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn export_to_png() {
+        let args = InkscapeArgsBuilder::new().png(true).build();
+        assert_eq!("inkscape --export-type=png", format!("{}", args));
+    }
+
+    #[test]
+    fn export_to_pdf() {
+        let args = InkscapeArgsBuilder::new().pdf(true).build();
+        assert_eq!("inkscape --export-type=pdf", format!("{}", args));
+    }
+
+    #[test]
+    fn export_to_eps() {
+        let args = InkscapeArgsBuilder::new().eps(true).build();
+        assert_eq!("inkscape --export-type=eps", format!("{}", args));
+    }
+
+    #[test]
+    fn export_to_all() {
+        let args = InkscapeArgsBuilder::new()
+            .png(true)
+            .pdf(true)
+            .eps(true)
+            .build();
+        assert_eq!("inkscape --export-type=png,pdf,eps", format!("{}", args));
+    }
+
+    #[test]
+    fn from_command_args_with_export_type_png() {
+        let mut cmd = std::process::Command::new("inkscape");
+        cmd.arg("--export-type=png");
+        let args = InkscapeArgs::from(cmd.get_args());
+        assert!(args.export_png);
+    }
+
+    #[test]
+    fn from_command_args_with_export_type_png_and_pdf() {
+        let mut cmd = std::process::Command::new("inkscape");
+        cmd.arg("--export-type=eps,png,pdf");
+        let args = InkscapeArgs::from(cmd.get_args());
+        assert!(args.export_png);
+        assert!(args.export_pdf);
+        assert!(args.export_eps);
+    }
 }
